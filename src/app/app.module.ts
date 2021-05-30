c033800fbd5844e0919f447f641a88dc63fa02ce
chaosg0hd <77721525+chaosg0hd@users.noreply.github.com>
2021-05-31 07:22:07 +0800

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,27 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 import { HttpClientModule } from '@angular/common/http';
+
+import { Camera } from '@ionic-native/Camera/ngx';
+import { File } from '@ionic-native/File/ngx';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
+import { IonicStorageModule } from '@ionic/storage';
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FontAwesomeModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FontAwesomeModule, Ng2SearchPipeModule,
+    IonicStorageModule.forRoot()],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Camera,
+    File,
+    WebView,
+    FilePath],
   bootstrap: [AppComponent],
 })
 export class AppModule {
