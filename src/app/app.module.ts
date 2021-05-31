08241e8063041aae305e1fa509d5fc8ea7345598
chaosg0hd <77721525+chaosg0hd@users.noreply.github.com>
2021-05-31 15:42:41 +0800

    3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -16,23 +16,25 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { HttpClientModule } from '@angular/common/http';
 
-import { Camera } from '@ionic-native/Camera/ngx';
-import { File } from '@ionic-native/File/ngx';
-import { WebView } from '@ionic-native/ionic-webview/ngx';
-import { FilePath } from '@ionic-native/file-path/ngx';
-import { IonicStorageModule } from '@ionic/storage';
+//import { Camera } from '@ionic-native/Camera/ngx';
+//import { File } from '@ionic-native/File/ngx';
+//import { WebView } from '@ionic-native/ionic-webview/ngx';
+//import { FilePath } from '@ionic-native/file-path/ngx';
+//import { IonicStorageModule } from '@ionic/storage';
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FontAwesomeModule, Ng2SearchPipeModule,
-    IonicStorageModule.forRoot()],
+    /*IonicStorageModule.forRoot()*/
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    Camera,
-    File,
-    WebView,
-    FilePath],
+    //Camera,
+    //File,
+    //WebView,
+    //FilePath
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
