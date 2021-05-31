5f7cbd244e87b429f4a21d9f2459686687366922
chaosg0hd <77721525+chaosg0hd@users.noreply.github.com>
2021-05-31 20:51:32 +0800

    13333

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,20 +1,23 @@
 import { IonicModule } from '@ionic/angular';
-import { NgModule, ChangeDetectorRef } from '@angular/core';
+import {
+  NgModule,
+  /*ChangeDetectorRef*/
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { File, FileEntry } from '@ionic-native/File/ngx';
-import { HttpClient } from '@angular/common/http';
-import { ActionSheetController, ToastController, Platform, LoadingController } from '@ionic/angular';
-import { WebView } from '@ionic-native/ionic-webview/ngx';
-import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/Camera/ngx';
-import { Storage } from '@ionic/storage';
-import { FilePath } from '@ionic-native/file-path/ngx';
-import { finalize } from 'rxjs/operators';
+//import { File, FileEntry } from '@ionic-native/File/ngx';
+//import { HttpClient } from '@angular/common/http';
+//import { ActionSheetController, ToastController, Platform, LoadingController } from '@ionic/angular';
+//import { WebView } from '@ionic-native/ionic-webview/ngx';
+//import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/Camera/ngx';
+//import { Storage } from '@ionic/storage';
+//import { FilePath } from '@ionic-native/file-path/ngx';
+//import { finalize } from 'rxjs/operators';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
-const STORAGE_KEY = 'my_images';
+//const STORAGE_KEY = 'my_images';
 
 @NgModule({
   imports: [
@@ -27,11 +30,11 @@ const STORAGE_KEY = 'my_images';
 })
 export class Tab1PageModule {
 
-  images = [];
+  //images = [];
 
-  constructor(private camera: Camera, private file: File, private http: HttpClient, private webview: WebView,
-    private actionSheetController: ActionSheetController, private toastController: ToastController,
-    private storage: Storage, private plt: Platform, private loadingController: LoadingController,
-    private ref: ChangeDetectorRef, private filePath: FilePath) { }
+  //constructor(private camera: Camera, private file: File, private http: HttpClient, private webview: WebView,
+  //  private actionSheetController: ActionSheetController, private toastController: ToastController,
+  //  private storage: Storage, private plt: Platform, private loadingController: LoadingController,
+  //  private ref: ChangeDetectorRef, private filePath: FilePath) { }
 
 }
